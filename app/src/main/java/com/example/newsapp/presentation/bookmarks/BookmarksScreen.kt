c51f2408ddad2229894bfd8a1609bcb2f685d2d0
sarthakvaswani <svas0000@gmai.com>
2025-01-19 17:40:50 +0530

    Added bookmarks navigation

---
+++ app/src/main/java/com/example/newsapp/presentation/bookmarks/BookmarksScreen.kt
@@ -0,0 +1,30 @@
+package com.example.newsapp.presentation.bookmarks
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.newsapp.presentation.home.NewsListView
+import com.example.newsapp.utils.NavRoute
+
+@Composable
+fun BookmarksScreen(navController: NavController) {
+    val viewModel : BookmarksViewModel = hiltViewModel()
+    val bookmarks = viewModel.getBookmarks().collectAsState(initial = listOf())
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text("Bookmarks", fontSize = 30.sp)
+        Spacer(modifier = Modifier.size(16.dp))
+        NewsListView(bookmarks.value, onClick = {
+            navController.navigate(NavRoute.createNewsDetailsRoute(it))
+        })
+    }
+
+}
---
+++ app/src/main/java/com/example/newsapp/presentation/bookmarks/BookmarksViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.newsapp.presentation.bookmarks
+
+import androidx.lifecycle.ViewModel
+import com.example.newsapp.data.database.NewsDB
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class BookmarksViewModel @Inject constructor(database: NewsDB) : ViewModel() {
+
+    private val newsDao = database.newsDAO()
+
+    fun getBookmarks() = newsDao.getNews()
+
+
+    //fun deleteBookmarks(news : News) = viewModelScope.launch {
+// newsDao.deleteNews(news)}
+
+
+}
